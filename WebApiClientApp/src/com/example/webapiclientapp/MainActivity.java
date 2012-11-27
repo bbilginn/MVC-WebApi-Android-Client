69cb5e14677dc3182134e6d20e3b3c1fd7e3a734
Berat Bilgin <bilginn@gmail.com>
2012-11-27 21:55:05 +0200

    Get gönder butonu hata sebebi ile değiştirildi.

--- WebApiClientApp/src/com/example/webapiclientapp/MainActivity.java
+++ WebApiClientApp/src/com/example/webapiclientapp/MainActivity.java
@@ -11,7 +11,6 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -30,39 +29,35 @@ public class MainActivity extends Activity {
 		Mail = (EditText) findViewById(R.id.txtMail);
 		Mesaj = (EditText) findViewById(R.id.txtMesaj);
 
-		GetGonder = (Button) findViewById(R.id.btnGetGonder);
+		final Button Gonder = (Button) findViewById(R.id.btnGetGonder);
 
 		NetKontrol NetConn = new NetKontrol();
 		if (!NetConn.isConn(this)) {
 			Dialogs.ToastGoster("Ýnternete eriþilemiyor.", MainActivity.this);
 		} else {
-			//GetGonder.setOnClickListener((OnClickListener) this);
-		}
-
-	}
-
-	public void onClick(View v) {
-
-		switch (v.getId()) {
-		case R.id.btnGetGonder:
-			try {
-				ThreadGet GetGonder = new ThreadGet();
-				GetGonder
-						.execute("http://webapiornek.azurewebsites.net/api/values");
-				Dialogs.ToastGoster("Gönderiliyor.", MainActivity.this);
-				GetGonder
-						.setDataDownloadListener(new ThreadGet.DataDownloadListener() {
+			//Get gönderisi
+			Gonder.setOnClickListener(new Button.OnClickListener() {
+				public void onClick(View v) {
+					try {
+						ThreadGet Get = new ThreadGet();
+						Get.execute("http://webapiornek.azurewebsites.net/api/values");
+						Dialogs.ToastGoster("Gönderiliyor.", MainActivity.this);
+						Get.setDataDownloadListener(new ThreadGet.DataDownloadListener() {
 							public void dataDownloadedSuccessfully(String data) {
 								Dialogs.ToastGoster("Ýþlem Baþarýlý, " + data,
 										MainActivity.this);
 							}
 						});
-			} catch (Exception e) {
-				Dialogs.ToastGoster("Ýþlem Baþarýsýz", MainActivity.this);
-			}
+					} catch (Exception e) {
+						Dialogs.ToastGoster("Ýþlem Baþarýsýz",
+								MainActivity.this);
+					}
 
-			break;
+				}
+			});
+
 		}
+
 	}
 
 	@Override
